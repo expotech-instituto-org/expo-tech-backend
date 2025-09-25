ceeb03545ab3e707c7752aee8ef8c97e0ab7a66e
EnricoPieroCosta <enrico.santos@germinare.org.br>
2025-09-25 08:42:55 -0300

    [feat] model knowledge

---
+++ app/model/knowledge.py
@@ -0,0 +1,16 @@
+import uuid
+from typing import Optional
+from pydantic import BaseModel, Field
+
+class KnowledgeModel(BaseModel):
+    id: Optional[str] = Field(alias="_id")
+    name: str = Field(..., description="Knowledge name")
+
+    class Config:
+        validate_by_name = True
+        json_schema_extra = {
+            "example": {
+                "_id": str(uuid.uuid4()),
+                "name": "admin",
+            }
+        }
