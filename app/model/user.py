0c88dde19917d2967d719bbc65eb8a03a3d9286f
kauarsantoss <kaua.santos@germinare.org.br>
2025-09-26 13:38:40 -0300

    backend(17): add review resume in user model

--- app/model/user.py
+++ app/model/user.py
@@ -3,6 +3,27 @@ from typing import Optional
 from pydantic import BaseModel, Field
 from app.model.role import RoleModel
 
+class ReviewResume(BaseModel):
+    project_id: str = Field(..., description="Project ID")
+    exhibition_id: str = Field(..., description="Exhibition ID")
+    comment: Optional[str] = Field(None, max_length=300, description="Review comment")
+    
+    class Config:
+        fields = {
+            "project_id": {"description": "Project ID"},
+            "exhibition_id": {"description": "Exhibition ID"},
+            "comment": {"description": "Optional comment about the review"}
+        }
+        allow_population_by_field_name = True
+        extra = "ignore"
+        json_schema_extra = {
+            "example": {
+                "project_id": str(uuid.uuid4()),
+                "exhibition_id": str(uuid.uuid4()),
+                "comment": "Excelente projeto, muito bem executado!"
+            }
+        }
+
 class UserModel(BaseModel):
     id: Optional[str] = Field(alias="_id")
     email: str = Field(...)
@@ -15,7 +36,7 @@ class UserModel(BaseModel):
     company: Optional[str] = Field(...)
     class_field: Optional[str] = Field(alias="class", default=None)
     # project: ProjectModel = Field(...)
-    # reviews: ReviewModel = Field(...)
+    reviews: ReviewResume = Field(...)
     active: bool = Field(default=True)
 
     class Config:
@@ -35,27 +56,5 @@ class UserModel(BaseModel):
                 "age": 30,
                 "class": "A",
                 "active": True,
-            }
-        }
-
-
-class ReviewResume(BaseModel):
-    project_id: str = Field(..., description="Project ID")
-    exhibition_id: str = Field(..., description="Exhibition ID")
-    comment: Optional[str] = Field(None, max_length=300, description="Review comment")
-    
-    class Config:
-        fields = {
-            "project_id": {"description": "Project ID"},
-            "exhibition_id": {"description": "Exhibition ID"},
-            "comment": {"description": "Optional comment about the review"}
-        }
-        allow_population_by_field_name = True
-        extra = "ignore"
-        json_schema_extra = {
-            "example": {
-                "project_id": str(uuid.uuid4()),
-                "exhibition_id": str(uuid.uuid4()),
-                "comment": "Excelente projeto, muito bem executado!"
             }
         }
