2465fe4e526e4dc9ca4841dbb511e8a5ad5868c4
kauarsantoss <kaua.santos@germinare.org.br>
2025-09-26 09:54:43 -0300

    backend(17): update user model

--- app/model/user.py
+++ app/model/user.py
@@ -1,11 +1,23 @@
 import uuid
 from typing import Optional
 from pydantic import BaseModel, Field
+from app.model.role import RoleModel
 
 class UserModel(BaseModel):
     id: Optional[str] = Field(alias="_id")
     login: str = Field(...)
+    phone: str = Field(...)
     senha: str = Field(...)
+    role: RoleModel = Field(...)
+    profile_picture: Optional[str] = Field(...)
+    knowledge: Optional[str] = Field(...)
+    age: Optional[int] = Field(...)
+    company: str = Field(...)
+    class_field: Optional[str] = Field(alias="class", default=None)
+    # project: ProjectModel = Field(...)
+    # reviews: ReviewModel = Field(...)
+    active: bool = Field(default=True)
+    
 
     class Config:
         validate_by_name = True
