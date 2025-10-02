3490096535791cd57cd5f5c3b205ac6fcab8e515
PedroSchettini <pedro.sousa@germinare.org.br>
2025-10-02 10:01:51 -0300

    Arrumando model de exhibition

--- app/model/exhibition.py
+++ app/model/exhibition.py
@@ -1,46 +1,37 @@
 import uuid
 from typing import Optional, List
-from datetime import date
 from pydantic import BaseModel, Field
-from app.model.role import RoleModel
-from app.model.project import ProjectModel
-
-class ProjectResume(ProjectModel):
-    class Config:
-        fields = {
-            "id": ..., 
-            "name": ...,
-            "logo": ..., 
-            "company_name": ...,  
-        }
-        extra = "ignore"
-
-class CriteriaModel(BaseModel):
-    name: str = Field(..., description="Criteria name")
-    weight: float = Field(..., description="Criteria weight")
-
-class RoleResume(RoleModel):
-    weight: float = Field(..., description="Role weight")
-    
-    class Config:
-        fields = {
-            "id": ...,
-            "name": ...,
-            "weight": ...,
-        }
-        extra = "ignore"
+from datetime import datetime
 
 class ExhibitionModel(BaseModel):
     id: Optional[str] = Field(alias="_id")
     name: str = Field(..., description="Exhibition name")
     image: str = Field(..., description="Exhibition image")
-    date: date = Field(..., description="Exhibition date")
+    date: datetime  = Field(..., description="Exhibition date")
     description: str = Field(..., description="Exhibition description")
-    roles: List[RoleResume] = Field(..., description="Exhibition roles")
-    projects: List[ProjectResume] = Field(..., description="Exhibition projects")
-    criteria: List[CriteriaModel] = Field(..., description="Exhibition criteria")
     active: bool = Field(..., description="Exhibition active")
-    
+
+    class ProjectResume(BaseModel):
+        id: Optional[str] = Field(alias="_id")
+        name: str = Field(..., description="Exhibition name")
+        logo: str = Field(..., description="Exhibition image")
+        company_name: str = Field(..., description="Exhibition name")
+
+    projects: List[ProjectResume] = Field(..., description="Exhibition projects")
+
+    class CriteriaModel(BaseModel):
+        name: str = Field(..., description="Criteria name")
+        weight: float = Field(..., description="Criteria weight")
+
+    criteria: List[CriteriaModel] = Field(..., description="Exhibition criteria")
+
+    class RoleResume(BaseModel):
+        id: Optional[str] = Field(alias="_id")
+        name: str = Field(..., description="Exhibition name")
+        weight: float = Field(..., description="Role weight")
+        
+    roles: List[RoleResume] = Field(..., description="Exhibition roles")
+
     class Config:
         validate_by_name = True
         json_schema_extra = {
@@ -48,7 +39,7 @@ class ExhibitionModel(BaseModel):
                 "_id": str(uuid.uuid4()),
                 "name": "Tech Corp",
                 "image": "https://br.pinterest.com/pin/345088390217105894/",
-                "date": "2025-01-01",
+                "date":  datetime(2025, 1, 1, 15, 30),
                 "description": "Exhibition description",
                 "roles": [
                     {
