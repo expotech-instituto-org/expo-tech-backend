da128fd28583f0d163ccf74aa40c708300595ca2
SophieKumagai <sophie.satie20@gmail.com>
2025-11-12 23:16:20 -0300

    exception

--- app/repository/user_repository.py
+++ app/repository/user_repository.py
@@ -160,6 +160,8 @@ async def update_user_basic_info(
     users_collection.update_one({"_id": user_id}, {"$set": updates})
 
     updated_user = users_collection.find_one({"_id": user_id})
+    if not updated_user:
+        raise HTTPException(status_code=404, detail="User not found after update")
     return UserModel(**updated_user)
 
 
